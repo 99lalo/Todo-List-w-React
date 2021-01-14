4cfb9efc3cd22da7e19940a8a8e0168fcebac46f
99lalo <xx66laloxx@gmail.com>
2021-01-14 02:01:00 +0000

    Update

--- src/js/component/list.js
+++ src/js/component/list.js
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-import { array } from "prop-types";
 
 //create your first component
 export function List() {
 	const [isShown, setIsShown] = useState(false);
 	const [task, setTask] = useState([]);
 
+	const handleRemoval = label => {
+		const newList = task.filter(item => item.label !== label);
+		setTask(newList);
+	};
 	return (
 		<div className="container">
+			<div className="d-flex justify-content-center">
+				<h1>todos</h1>
+			</div>
 			<ul className="list-group">
 				<li className="list-group-item">
 					<input
@@ -19,26 +24,31 @@ export function List() {
 							(setTask(task.concat({ label: e.target.value })),
 							(e.target.value = ""))
 						}
+						placeholder="What needs to be done?"
+						style={{ border: "none" }}
 					/>
 				</li>
-				{task === null ? (
+				{task.length == 0 ? (
 					<li className="list-group-item">No tasks, add a task</li>
 				) : (
 					task.map(t => (
 						<li
 							className="list-group-item"
-							onMouseEnter={() => setIsShown(true)}
+							onMouseEnter={() => setIsShown(t.label)}
 							onMouseLeave={() => setIsShown(false)}
 							key={t.id}>
 							{t.label}
-							{isShown && (
-								<span style={{ color: "red", float: "right" }}>
+							{isShown == t.label && (
+								<span
+									style={{ float: "right" }}
+									onClick={() => handleRemoval(t.label)}>
 									X
 								</span>
 							)}
 						</li>
 					))
 				)}
+				<li className="list-group-item">{task.length} items left</li>
 			</ul>
 		</div>
 	);
